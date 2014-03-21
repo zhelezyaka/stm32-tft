f7ff8a68cf2a2697b71544ccdb013c4a87d7307d
solee.linux@gmail.com <solee.linux@gmail.com@48509b87-dd8d-514d-b9c1-9a831c9cefe0>
2014-03-21 17:34:59 +0000

    added show string function.

--- User/lcd.h
+++ User/lcd.h
@@ -1,6 +1,8 @@
 #ifndef _SOLEE_LCD_H_
 #define _SOLEE_LCD_H_
 
+#include <stdint.h>
+
 //常用颜色值定义
 #define COLOR_WHITE          0xFFFF
 #define COLOR_BLACK          0x0000
@@ -16,6 +18,10 @@
 //驱动Ic
 static u16 _deviceCode;
 
+static volatile u16 _color = COLOR_BLACK, _background = COLOR_BLUE;
+
+#define FONT  Font16x24
+
 /*
 NE1 0x600000000
 NE2 0x640000000
@@ -23,8 +29,8 @@ NE3 0x680000000
 NE4 0x6C0000000
 */
 
-#define ili9325_REG          (*((volatile unsigned short *) 0x60000000)) 
-#define ili9325_RAM          (*((volatile unsigned short *) 0x60020000)) // A16(RS)
+#define ili9325_REG          (*((volatile u16 *) 0x60000000)) 
+#define ili9325_RAM          (*((volatile u16 *) 0x60020000)) // A16(RS)
 
 //系统初始化
 void initGPIO(void);
@@ -41,7 +47,12 @@ void lcdReset(void);
 void lcdSetCursor(u16 x,u16 y);
 void lcdClear(u16 color);
 
-unsigned short lcdReadGRAM(unsigned short x,unsigned short y);
+void lcdSetColor(u16 color, u16 background);
+void lcdDrawChar(u32 x, u32 y, const u16 *c);
+void lcdShowChar(u32 ln, u32 col, u16 c);
+void lcdShowString(u32 x, u32 y, const char *str);
+
+unsigned short lcdReadGRAM(u16 x,u16 y);
 
 #endif
 
